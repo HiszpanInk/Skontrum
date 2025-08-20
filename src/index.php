3ac6fe172d8e40772e080dc95622f5b1215b88da
HiszpanInk <wojtek.szacon@gmail.com>
2025-08-20 20:22:04 +0200

    First basic pages, working on adding books, database structure will be provided in the near future

---
+++ src/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Skontrum</title>
+</head>
+<body>
+    <main>
+        <h1>Welcome to Skontrum! Open-source library system written in pure PHP.</h1>
+        <p>Name comes from polish word <i>skontrum</i> which is a term librarians (and law) use, referring to inventarisation in the context of a library.</p>
+    </main>
+    <footer>© HiszpanInk 2025, <a target="_blank" href="https://github.com/HiszpanInk/Skontrum">source code</a> is licensed under <a target="_blank" href="https://interoperable-europe.ec.europa.eu/sites/default/files/custom-page/attachment/2020-03/EUPL-1.2%20EN.txt">EUPL 1.2</a></footer>
+</body>
+</html>
